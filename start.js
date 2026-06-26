cdc65ac424c8859613c5af952b3711fb1fb4d0f5
Kirikou3008 <baptiste.paixao@gmail.com>
2026-06-26 09:52:53 +0200

    Update start.js

--- start.js
+++ start.js
@@ -13,7 +13,6 @@
   q51_activitesA51, q64_already_paid
 */
 
-const WEBHOOK_URL = "https://baptistepaixao2.app.n8n.cloud/webhook/voyage-form";
 const WEBHOOK_URL = "https://baptistepaixao2.app.n8n.cloud/webhook-test/voyage-form";
 const SKIP_PAYMENT_FOR_TEST = true;
 const MAX_TRAVELLERS = 10;
